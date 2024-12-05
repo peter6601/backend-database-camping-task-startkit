b0b573004ab01fb0a8d8337e8ce3233f4129bda3
DinDin <peter779701@gmail.com>
2024-12-05 18:07:25 +0800

    [Update] 留task 1, 2 註解其餘的

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -52,47 +52,47 @@ select * from "USER"
 limit 3;
 
 
--- --  ████████  █████   █    ████  
--- --    █ █   ██    █  █         █ 
--- --    █ █████ ███ ███       ███  
--- --    █ █   █    ██  █     █     
--- --    █ █   █████ █   █    █████ 
--- -- ===================== ====================
--- -- 2. 組合包方案 CREDIT_PACKAGE、客戶購買課程堂數 CREDIT_PURCHASE
--- -- 2-1. 新增：在`CREDIT_PACKAGE` 資料表新增三筆資料，資料需求如下：
---     -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
---     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
---     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
+--  ████████  █████   █    ████  
+--    █ █   ██    █  █         █ 
+--    █ █████ ███ ███       ███  
+--    █ █   █    ██  █     █     
+--    █ █   █████ █   █    █████ 
+-- ===================== ====================
+-- 2. 組合包方案 CREDIT_PACKAGE、客戶購買課程堂數 CREDIT_PURCHASE
+-- 2-1. 新增：在`CREDIT_PACKAGE` 資料表新增三筆資料，資料需求如下：
+    -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
+    -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
+    -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
 
--- INSERT into "CREDIT_PACKAGE" (name, credit_amount, price) VAlUES
---         ('7 堂組合包方案', 7, 1400),
---         ('14 堂組合包方案', 14, 2520),
---         ('21 堂組合包方案', 21, 4800);
+INSERT into "CREDIT_PACKAGE" (name, credit_amount, price) VAlUES
+        ('7 堂組合包方案', 7, 1400),
+        ('14 堂組合包方案', 14, 2520),
+        ('21 堂組合包方案', 21, 4800);
 
 
--- -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
---     -- 1. `王小明` 購買 `14 堂組合包方案`
---     -- 2. `王小明` 購買 `21 堂組合包方案`
---     -- 3. `好野人` 購買 `14 堂組合包方案`
--- INSERT into "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VAlUES
--- (
---     (select id from "USER" where name = '王小明'), 
---     (select id from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
---     (select credit_amount from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
---     (select price from "CREDIT_PACKAGE" where name = '14 堂組合包方案')
---  ),
---  (
---     (select id from "USER" where name = '王小明'), 
---     (select id from "CREDIT_PACKAGE" where name = '21 堂組合包方案'),
---     (select credit_amount from "CREDIT_PACKAGE" where name = '21 堂組合包方案'),
---     (select price from "CREDIT_PACKAGE" where name = '21 堂組合包方案')
---  ),
---  (
---     (select id from "USER" where name = '好野人'), 
---     (select id from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
---     (select credit_amount from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
---     (select price from "CREDIT_PACKAGE" where name = '14 堂組合包方案')
---  );
+-- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
+    -- 1. `王小明` 購買 `14 堂組合包方案`
+    -- 2. `王小明` 購買 `21 堂組合包方案`
+    -- 3. `好野人` 購買 `14 堂組合包方案`
+INSERT into "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VAlUES
+(
+    (select id from "USER" where name = '王小明'), 
+    (select id from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
+    (select credit_amount from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
+    (select price from "CREDIT_PACKAGE" where name = '14 堂組合包方案')
+ ),
+ (
+    (select id from "USER" where name = '王小明'), 
+    (select id from "CREDIT_PACKAGE" where name = '21 堂組合包方案'),
+    (select credit_amount from "CREDIT_PACKAGE" where name = '21 堂組合包方案'),
+    (select price from "CREDIT_PACKAGE" where name = '21 堂組合包方案')
+ ),
+ (
+    (select id from "USER" where name = '好野人'), 
+    (select id from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
+    (select credit_amount from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
+    (select price from "CREDIT_PACKAGE" where name = '14 堂組合包方案')
+ );
 
 
 
